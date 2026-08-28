dc27d7fe1ee026372122f555a13b11dc870cf2ef
quillaja <ben@dh>
2026-08-28 16:14:20 -0700

    add files

---
+++ script.js
@@ -0,0 +1,111 @@
+class App {
+
+    constructor() {
+        this.locations = [];
+
+        this.map = L.map("map").setView([0, 0], 16);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.map);
+
+        this.currentLocationMarker = undefined;
+        this.historyPath = undefined;
+
+        this.watchHandle = undefined;
+    }
+
+    /**
+     * @param {GeolocationPosition} position 
+     */
+    addPosition(position) {
+        this.locations.push(position);
+        this._updatePath();
+        this._updateCircle();
+    }
+
+    _updateCircle() {
+
+        if (this.locations.length < 1)
+            return;
+
+        const p = this.locations.at(-1);
+        const latlon = [p.coords.latitude, p.coords.longitude];
+        const radius = p.coords.accuracy;
+
+        if (this.currentLocationMarker)
+            this.currentLocationMarker.remove();
+
+        this.currentLocationMarker = L.circle(
+            latlon, {
+            color: "#0af",
+            fillColor: "#0af",
+            fillOpacity: 0.25,
+            radius: radius
+        }).addTo(this.map);
+
+        this.map.flyTo(latlon);
+    }
+
+    _updatePath() {
+
+        if (this.locations.length < 2)
+            return;
+
+        if (this.historyPath === undefined) {
+            const coords = this.locations.map(p => [p.coords.latitude, p.coords.longitude]);
+            this.historyPath = L.polyline(
+                coords, {
+                color: "#0af",
+                opacity: 0.20
+            }).addTo(this.map);
+        }
+        else {
+            const p = this.locations.at(-1);
+            const latlon = [p.coords.latitude, p.coords.longitude];
+            this.historyPath.addLatLng(latlon);
+        }
+    }
+
+    saveLocations() {
+        const json = JSON.stringify(this.locations);
+        var blob = new Blob([json], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, "locations.json"); // FileSaver.js
+    }
+
+    startWatch() {
+        this.stopWatch();
+
+        this.watchHandle = navigator.geolocation.watchPosition(
+            (position) => {
+                console.log(position);
+                this.addPosition(position);
+            },
+            (err) => {
+                console.error(err);
+            },
+            { enableHighAccuracy: true }
+        );
+
+        console.log(`started watch ${this.watchHandle}`)
+    }
+
+    stopWatch() {
+        if (this.watchHandle !== undefined) {
+            navigator.geolocation.clearWatch(this.watchHandle);
+            console.log(`cleared watch ${this.watchHandle}`)
+            this.watchHandle = undefined;
+        }
+    }
+};
+
+window.addEventListener("load", () => {
+
+    const app = new App();
+
+    document.getElementById("btnStart").addEventListener("click", () => app.startWatch());
+    document.getElementById("btnStop").addEventListener("click", () => app.stopWatch());
+    document.getElementById("btnSave").addEventListener("click", () => app.saveLocations());
+
+    console.log("started app");
+});
