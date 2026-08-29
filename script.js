0864d628409caf1b52b41450f1b35d5a53aa1a70
Ben <quillaja@hotmail.com>
2026-08-28 17:57:03 -0700

    Revert "increase max zoom"
    This reverts commit d0907a3fe7181d9adbe15300a85b82b9ac5222d4.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class App {
 
         this.map = L.map("map").setView([0, 0], 16);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 22,
+            maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
 
